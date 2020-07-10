767ccc0ceb3f2d8e3eb66b5baf5aa0be55fd6e71
Diastowo <diastowo@gmail.com>
2020-07-10 17:40:29 +0700

    add realtime

---
+++ public/javascripts/lamp_fe.js
@@ -0,0 +1,31 @@
+setInterval(function() {
+	console.log('hai')
+
+	$.ajax({
+        url: '/lamp/all',
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+        	console.log(data)
+        	for (var i=0; i<data.length; i++) {
+        		$('#lamp_' + data[i].lamp_id).html(data[i].lamp_current + ' Amp')
+        	}
+        },
+    });
+
+
+	$.ajax({
+        url: '/lamp/volt/all',
+        type: 'get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+        	console.log(data)
+
+        	for (var i=0; i<data.length; i++) {
+        		$('#volt_' + data[i].volt_id).html(data[i].volt + ' Volt')
+        	}
+        },
+    });
+}, 5000)
